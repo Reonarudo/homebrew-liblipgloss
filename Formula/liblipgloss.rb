c910920fdbc50ff49fe3d996eafc4b7ab4e89af3
Leonardo Marques <leonardo.j.marques@gmail.com>
2025-02-01 20:52:26 +0100

    chore: updated sha256

--- Formula/liblipgloss.rb
+++ Formula/liblipgloss.rb
@@ -2,7 +2,7 @@ class Liblipgloss < Formula
   desc "C Wrapper for Lipgloss, a terminal UI styling library"
   homepage "https://github.com/Reonarudo/liblipgloss"
   url "https://github.com/Reonarudo/liblipgloss/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "10dc49309cd23d799095a873531c346ae50fc1fd898993abc7d9f200e2bd5a32"
+  sha256 "453d2bae9ec6d7b32c8aa3ea8edfa6e0882cdd6bdec70df383e32774c6ab7e63"
   license "MIT"
 
   depends_on "go" => :build
