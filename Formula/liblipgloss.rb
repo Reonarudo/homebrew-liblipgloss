793f41f517c424fd15a06b5d998a363ddcb63950
Leonardo Marques <leonardo.j.marques@gmail.com>
2025-02-01 20:39:18 +0100

    "Add liblipgloss formula"

---
+++ Formula/liblipgloss.rb
@@ -0,0 +1,21 @@
+class Liblipgloss < Formula
+  desc "C Wrapper for Lipgloss, a terminal UI styling library"
+  homepage "https://github.com/Reonarudo/liblipgloss"
+  url "https://github.com/Reonarudo/liblipgloss/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "10dc49309cd23d799095a873531c346ae50fc1fd898993abc7d9f200e2bd5a32"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on "clang" => :build
+
+  def install
+    ENV["CGO_CFLAGS"] = "-I#{include}"
+    system "make"
+    lib.install "liblipgloss.dylib" # Libraries go to lib, not bin
+    include.install Dir["include/*.h"]
+  end
+
+  test do
+    system "test", "-f", "#{lib}/liblipgloss.dylib"
+  end
+end
