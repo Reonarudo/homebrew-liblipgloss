1d954296c2b2a2b27b937f312b9bd6da3f69dadb
Leonardo Marques <leonardo.j.marques@gmail.com>
2025-02-01 22:47:02 +0100

    chore: updated to include .pc file creation

--- Formula/liblipgloss.rb
+++ Formula/liblipgloss.rb
@@ -2,19 +2,22 @@ class Liblipgloss < Formula
   desc "C Wrapper for Lipgloss, a terminal UI styling library"
   homepage "https://github.com/Reonarudo/liblipgloss"
   url "https://github.com/Reonarudo/liblipgloss/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "453d2bae9ec6d7b32c8aa3ea8edfa6e0882cdd6bdec70df383e32774c6ab7e63"
+  sha256 "62d98a4d68028b5cf8b9f10d50689689320249a8de5da1d186b18a7a13b676ca"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    ENV["CGO_CFLAGS"] = "-I#{include}"
     system "make"
-    lib.install "liblipgloss.dylib" # Libraries go to lib, not bin
-    include.install Dir["include/*.h"]
+    system "make", "install", "PREFIX=#{prefix}"
+
+    # Ensure Homebrew links correctly
+    ENV["CFLAGS"] = "-I#{include}"
+    ENV["LDFLAGS"] = "-L#{lib}"
   end
 
   test do
     system "test", "-f", "#{lib}/liblipgloss.dylib"
+    system "pkg-config", "--exists", "liblipgloss"
   end
 end
